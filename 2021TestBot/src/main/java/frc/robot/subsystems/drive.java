8c76d583d6c80ef732c9df06892c6a456c2ac6e9
BrazillDazzle <66147067+BrazillDazzle@users.noreply.github.com>
2021-03-30 19:04:09 -0400

    changes

---
+++ 2021TestBot/src/main/java/frc/robot/subsystems/drive.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+/** Add your docs here. */
+public class drive extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  WPI_VictorSPX leftDrive = new WPI_VictorSPX(0);
+  WPI_VictorSPX rightDrive = new WPI_VictorSPX(1);
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
