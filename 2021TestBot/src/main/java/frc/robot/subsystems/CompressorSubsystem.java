669209e154627931ecf95408ce6fad0dee367e1c
FlintWeststeel <59623576+FlintWeststeel@users.noreply.github.com>
2021-06-12 08:33:59 -0400

    up

--- 2021TestBot/src/main/java/frc/robot/subsystems/CompressorSubsystem.java
+++ 2021TestBot/src/main/java/frc/robot/subsystems/CompressorSubsystem.java
@@ -1,39 +1,38 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import javax.swing.JToggleButton.ToggleButtonModel;
 public class CompressorSubsystem extends SubsystemBase {
 
-    private WPI_VictorSPX leftDrive = new WPI_VictorSPX(Constants.MOTOR_LEFT_ID);
-    private WPI_VictorSPX rightDrive = new WPI_VictorSPX(Constants.MOTOR_RIGHT_ID);
+    public Compressor compressor = new Compressor(0);
+    boolean bool = false;
+    boolean value;
+    public void compressorControl(Joystick controller){
+       /* if(controller.getRawButton(12)){
+            if(bool){
+                bool = false;
+            }
+            else{
+                bool = true;
+            }
+        }*/
+        if(controller.getRawButton(12)){
+            compressor.start();
 
-    private SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightDrive);
-    private SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftDrive);
-
-    public DifferentialDrive base = new DifferentialDrive(leftMotors, rightMotors);
-
-    public void drive(double speed, double angle) {
-        System.out.println("Drive");
-        base.arcadeDrive(speed, angle);
+        }
+        else{
+            compressor.stop();
+        }
+    
+    }
+    public boolean compressorValue(){
+        value = compressor.getPressureSwitchValue();
+        return value;
     }
 
-    public void initDefaultCommand() {
-        // setDefaultCommand(new Drive(driveController.getRawAxis(1),
-        // driveController.getRawAxis(0));
-    }
-
-    public void setLeftMotors(double speed) {
-        leftDrive.set(ControlMode.PercentOutput, -speed);
-    }
-
-    public void setRightMotors(double speed) {
-        rightDrive.set(ControlMode.PercentOutput, speed);
+    public void initDefaultCommand(){
     }
 }
