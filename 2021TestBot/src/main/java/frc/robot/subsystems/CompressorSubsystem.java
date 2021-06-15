b6380bf9cdd82481699d7b3a7ed86faf2c2a47c7
FlintWeststeel <59623576+FlintWeststeel@users.noreply.github.com>
2021-06-15 19:04:50 -0400

    update

--- 2021TestBot/src/main/java/frc/robot/subsystems/CompressorSubsystem.java
+++ 2021TestBot/src/main/java/frc/robot/subsystems/CompressorSubsystem.java
@@ -5,28 +5,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
 import javax.swing.JToggleButton.ToggleButtonModel;
-public class CompressorSubsystem extends SubsystemBase {
+import javax.swing.plaf.basic.BasicBorders.ToggleButtonBorder;
+public class CompressorSubsystem extends SubsystemBase{
 
     public Compressor compressor = new Compressor(0);
     boolean bool = false;
     boolean value;
+    ToggleButtonModel button = new ToggleButtonModel();
     public void compressorControl(Joystick controller){
-       /* if(controller.getRawButton(12)){
-            if(bool){
-                bool = false;
-            }
-            else{
-                bool = true;
-            }
-        }*/
+       
         if(controller.getRawButton(12)){
             compressor.start();
-
         }
-        else{
+        if(controller.getRawButton(11)){
             compressor.stop();
         }
-    
+            
+        
     }
     public boolean compressorValue(){
         value = compressor.getPressureSwitchValue();
--- 2021TestBot/src/main/java/frc/robot/subsystems/ShooterTurnSubsystem.java
+++ 2021TestBot/src/main/java/frc/robot/subsystems/ShooterTurnSubsystem.java
@@ -6,26 +6,49 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 
 public class ShooterTurnSubsystem extends SubsystemBase {
 
   public WPI_VictorSPX shooterTurnDrive = new WPI_VictorSPX(Constants.MOTOR_SHOOTERTURN_ID);
-
+  DigitalInput leftLimitSwitch = new DigitalInput(0);
+  DigitalInput rightLimitSwitch = new DigitalInput(1);
+  String string;
   /** Creates a new ShooterTurnSubsystem. */
   public void ShooterTurn(Joystick controller){
-    if(controller.getRawButton(3)){
-      shooterTurnDrive.set(Constants.SHOOTER_TURN_SPEED);
+    //Checks last movement       
+
+    if(!rightLimitSwitch.get() && !leftLimitSwitch.get()){//checks limit switch hit
+      if(controller.getRawButton(3)){
+        shooterTurnDrive.set(Constants.SHOOTER_TURN_SPEED);
+      }
+      else if(controller.getRawButton(4)){
+        shooterTurnDrive.set(-Constants.SHOOTER_TURN_SPEED);
+      }
+      else{
+        shooterTurnDrive.set(0);
+      }
     }
-    else if(controller.getRawButton(4)){
-      shooterTurnDrive.set(-Constants.SHOOTER_TURN_SPEED);
+    else if(leftLimitSwitch.get()){ //hits limit switch while turning left
+      if(controller.getRawButton(4)){
+        shooterTurnDrive.set(-Constants.SHOOTER_TURN_SPEED);
+      }
+      else{
+        shooterTurnDrive.set(0);
+      }
     }
-    else{
-      shooterTurnDrive.set(0);
+    else if(rightLimitSwitch.get()){//hits limit switch while turning right
+      if(controller.getRawButton(3)){
+        shooterTurnDrive.set(Constants.SHOOTER_TURN_SPEED);
+      }
+      else{
+        shooterTurnDrive.set(0);
+      }
     }
+
   }
   @Override
   public void periodic() {
